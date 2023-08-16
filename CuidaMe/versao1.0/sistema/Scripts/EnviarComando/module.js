d531b7dfde7a5f8eeda027b54188ce4a717dbcfe
Elton J. De Assunção <ewa.soft@gmail.com>
2023-08-15 21:08:31 -0300

    version 1.0.2

---
+++ CuidaMe/versao1.0/sistema/Scripts/EnviarComando/module.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    const app = angular.module("app");
+
+    app.controller("EnviarComandoController", function ($scope, $messages, $enviarcomando) {
+        $scope.salvar = function () {
+            $enviarcomando.save($scope.enviarcomando, function () {
+            });
+        };
+    });
+})();
